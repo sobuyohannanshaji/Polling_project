8c3dd141b2b6f11db2b91cc14ca523674ac584a0
sobu yohannan shaji <sobu003@gmail.com>
2023-01-29 01:48:57 +0530

    Commit

--- poll/settings.py
+++ poll/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-35vxqkm9g2ldfszoo)2y*6dh5#nbi)y24xg(1w_ds8*@o&xen8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pollingproject-production.up.railway.app']
 
